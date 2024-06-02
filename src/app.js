f4895b1786592e2c30e660fa6943c3652b265c70
ThaiChinh1905 <thaichinh1905@gmail.com>
2024-06-02 17:01:17 +0700

    init

---
+++ src/app.js
@@ -0,0 +1,491 @@
+const express = require('express');
+const https = require('httpolyglot');
+const fs = require('fs');
+const path = require('path');
+const cors = require("cors");
+const { Server } = require('socket.io');
+const mediasoup = require('mediasoup');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+const app = express();
+const config = require('./config');
+const FFmpeg = require('./ffmpeg');
+const direcLink = require('./directLink')
+const {
+  getPort,
+} = require('./port');
+const { ppid } = require('process');
+
+app.use(cors("*"))
+app.use('/play', express.static('../client/public'))
+app.use('/webplay', express.static('../webclient/src'))
+
+app.use('/playhls', (request, response) => {
+  const url = request.url.substring(request.url.lastIndexOf('/') + 1);
+  const base = path.basename(url, path.extname(url))
+  const extractBase = base.substring(0, base.indexOf('-hls') + 4);
+  let filePath = ""
+  var filePathOption1 = path.resolve(`../server/files/hls/${base}/${url}`);
+  var filePathOption2 = path.resolve(`../server/files/hls/${extractBase}/${url}`)
+
+  if (fs.existsSync(filePathOption1)) {
+      filePath = filePathOption1
+  }
+  else {
+      filePath = filePathOption2
+  }
+
+  fs.readFile(filePath, function (error, content) {
+      response.writeHead(200, { 'Access-Control-Allow-Origin': '*' });
+      if (error) {
+          if (error.code == 'ENOENT') {
+              fs.readFile('./404.html', function (error, content) {
+                  response.end(content, 'utf-8');
+              });
+          }
+          else {
+              response.writeHead(500);
+              response.end(' error: ' + error.code + ' ..\n');
+              response.end();
+          }
+      }
+      else {
+          response.end(content, 'utf-8');
+      }
+  });
+})
+
+const options = {
+  key: fs.readFileSync('./ssl/key.pem', 'utf-8'),
+  cert: fs.readFileSync('./ssl/cert.pem', 'utf-8')
+}
+
+const httpsServer = https.createServer(options, app)
+
+const PORT = process.env.PORT;
+const HOST_IP = process.env.HOST_IP;
+const DEVICE_IP = process.env.DEVICE_IP;
+httpsServer.listen(PORT, () => {
+  console.log('listening on port: ' + PORT)
+})
+
+const io = new Server(httpsServer, {
+  allowEIO3: true,
+  cors: {
+    origin: "*"
+  }
+})
+
+const peers = io.of('/mediasoup')
+
+let worker
+let router
+let producerTransport
+let consumerTransport
+let producers = new Map()
+let producerFails = new Set();
+let consumers = []
+const peer = {};
+let webRTCTransport = []
+let processWriteHLS = {};
+let consumer;
+const mediaCodecs = [
+  {
+    kind: 'audio',
+    mimeType: 'audio/opus',
+    preferredPayloadType: 111,
+    clockRate: 48000,
+    channels: 2,
+    parameters: {
+      minptime: 10,
+      useinbandfec: 1,
+    }
+  },
+  {
+    kind: 'audio',
+    mimeType: 'audio/opus',
+    clockRate: 48000,
+    payloadType: 101,
+    channels: 2,
+    parameters: { 'sprop-stereo': 1 },
+    rtcpFeedback: [
+      { type: 'transport-cc' },
+    ],
+  },
+  {
+    kind: 'video',
+    mimeType: 'video/VP8',
+    clockRate: 90000,
+    parameters: {
+      'x-google-start-bitrate': 1000,
+    },
+  },
+]
+
+
+const createWorker = async () => {
+  worker = await mediasoup.createWorker({
+    rtcMinPort: 20000,
+    rtcMaxPort: 30000,
+  })
+  console.log(`worker pid ${worker.pid}`)
+
+  worker.on('died', error => {
+    console.error('mediasoup worker has died');
+    setTimeout(() => process.exit(1), 2000);
+  })
+  router = await worker.createRouter({ mediaCodecs })
+  return router
+}
+
+const createPlain = async () => {
+
+  const streamTransport = await router.createPlainTransport({
+    listenInfo: {
+      protocol: "udp",
+      ip: '0.0.0.0',
+      announcedIp: HOST_IP,
+      // port: 26000
+    },
+    rtcpMux: true,
+    comedia: true,
+  });
+
+  return streamTransport;
+}
+router = createWorker()
+
+const getProducer = (channelSlug) => {
+  if (!producers.has(channelSlug) || producers.get(channelSlug).length==0) {
+    return null
+  }
+  return producers.get(channelSlug)[0]
+}
+
+// const getProducerList = () => {
+//   return producers
+//   let producersList = {}
+//   for ([key, value] in producers) {
+
+//     producersList[key] = {
+//       id: value.id,
+//       slug: value.slug
+//     }
+//   }
+//   return producersList
+// }
+
+peers.on('connection', async socket => {
+
+  socket.on('disconnect', () => {
+    if (processWriteHLS[socket.id]) {
+      processWriteHLS[socket.id].kill()
+      delete processWriteHLS[socket.id]
+    }
+    webRTCTransport = webRTCTransport.filter(item => item.id === socket.id);
+    console.log('peer disconnected')
+  })
+
+  socket.on('createRoom', async (callback) => {
+    if (router === undefined) {
+      router = await worker.createRouter({ mediaCodecs })
+      console.log(`Router ID: ${router.id}`)
+    }
+
+    getRtpCapabilities(callback)
+  })
+
+  const getRtpCapabilities = (callback) => {
+    const rtpCapabilities = router.rtpCapabilities;
+    callback({ rtpCapabilities })
+  }
+
+  socket.on('createWebRtcTransport', async ({ sender }, callback) => {
+    console.log(`Is this a sender request? ${sender}`)
+    if (sender) {
+      producerSocketId = socket.id
+      producerTransport = await createWebRtcTransport(callback)
+    }
+    else {
+      consumerSocketId = socket.id
+      consumerTransport = await createWebRtcTransport(callback)
+      const existsIndex = webRTCTransport.findIndex(item => item.id === socket.id);
+      if (existsIndex !== -1) {
+        webRTCTransport[existsIndex].consumerTransport = consumerTransport;
+      } else {
+        webRTCTransport.push({
+          consumerTransport,
+          id: socket.id
+        })
+      }
+    }
+  })
+
+  // socket.on('record', async () => {
+  //   startRecord(peer)
+  // })
+
+  socket.on('transport-recv-connect', async ({ dtlsParameters }) => {
+    console.log("=====================================::")
+    const consumerTransport = webRTCTransport.find(item => item.id == socket.id).consumerTransport
+    await consumerTransport.connect({ dtlsParameters })
+  })
+
+  socket.on('consume', async ({ rtpCapabilities, channelSlug }, callback) => {
+    try {
+      if (!channelSlug) {
+        throw new Error(`Invalid channel:${channelSlug}`)
+      }
+      socket.join(channelSlug);
+      let producer;
+      if (producers.get(channelSlug) && producers.get(channelSlug).length > 0) {
+        producer = producers.get(channelSlug)[0].producer;
+      }
+      if (!producer) {
+        throw new Error(`Cannot find producer for channel ${channelSlug}`)
+      }
+      if (router.canConsume({
+        producerId: producer.id,
+        rtpCapabilities
+      })) {
+        const consumerTransport = webRTCTransport.find(item => item.id == socket.id).consumerTransport;
+        const consumer = await consumerTransport.consume({
+          producerId: producer.id,
+          rtpCapabilities,
+          paused: true,
+        })
+
+        const params = {
+          id: consumer.id,
+          producerId: producer.id,
+          kind: consumer.kind,
+          rtpParameters: consumer.rtpParameters,
+        }
+        await consumer.resume()
+        callback({ params })
+      }
+    } catch (error) {
+      callback({
+        params: {
+          error: error
+        }
+      })
+    }
+  })
+
+  // socket.on('consumer-resume', async () => {
+  //   console.log('consumer resume')
+
+  // })
+  socket.on('create-producer', async (data, callback) => {
+    const streamTransport = await createPlain();
+    const producer = await streamTransport.produce({
+      kind: 'audio',
+      rtpParameters: {
+        codecs: [{
+          mimeType: 'audio/opus',
+          clockRate: 48000,
+          payloadType: 101,
+          channels: 2,
+          parameters: { 'sprop-stereo': 1 },
+          rtcpFeedback: [
+            { type: 'transport-cc' },
+          ],
+        }],
+        encodings: [{ ssrc: 11111111 }],
+      },
+      appData: {},
+    });
+    // const isPlay = producers.find(item => item.slug === data.slug) 
+    // const existsIndex = producers.findIndex(item => item.id === data.id);
+    // if (existsIndex !== -1) {
+    //     producers[existsIndex].producer = producer;
+    // } else {
+    //   producers.push({ channelName: data.channelName, slug: data.slug, id: data.id, producer });
+    // }
+    if (!producers.has(data.slug)) {
+      producers.set(data.slug, [])
+    }
+    if(!producers.has(data.slug) || producers.get(data.slug).length === 0) {
+      startRecord(producer, data.slug, socket.id)
+    }
+    producers.get(data.slug).push(
+      {
+        slug: data.slug,
+        id: data.id,
+        producer: producer
+      }
+    )
+    console.log('Producers', producers);
+    callback(streamTransport.tuple.localPort)
+  })
+
+  socket.on("link-stream", async (data) => {
+    const { producer, transport } = await direcLink(router, data)
+    // console.log('prod,tran',producer,transport);
+    
+    if (!producers.has(data.channelName)) {
+      producers.set(data.channelName, [])
+      // startRecord(producer, data.slug, socket.id)
+    }
+    producers.get(data.channelName).push(
+      {
+        slug: data.slug,
+        id: data.id,
+        producer: producer,
+        active: true
+      }
+    )
+    console.log('Producers', producers);
+  } )
+
+})
+
+
+setInterval(async () => {
+  let countDelete = 0;
+  const promises = [];
+  const producerFails = [];
+  for (let [key, value] of producers) {
+    value.forEach(item => {
+      if (item.producer) {
+        promises.push(item.producer.getStats().then(stats => {
+          if (!stats ||stats[0]?.bitrate === 0) {
+            item.isDelete = true;
+            countDelete += 1;
+            producerFails.push(item.slug)
+            peers.to(item.slug).emit('reconnect');
+            if (consumerTransport && !consumerTransport.closed) {
+              consumerTransport.close();
+            }
+          }
+        }));
+      }
+    });
+
+  }
+
+
+  Promise.all(promises)
+  .then(() => {
+    if (countDelete > 0) {
+      for (let [key, value] of producers) {
+        const newValue = value.filter(data => data.isDelete !== true);
+        producers.set(key, newValue);
+      }
+    }
+    if(producerFails.length > 0) {
+      producerFails.forEach(item => {
+        const data = getProducer(item)
+        console.log(data)
+        if(data) {
+          startRecord(data.producer, item, data.socketId)
+        }
+      })
+    }
+  })
+  }, 1000)
+ 
+
+const createWebRtcTransport = async (callback) => {
+  try {
+    const webRtcTransport_options = {
+      listenIps: [
+        {
+          ip: '0.0.0.0',
+          announcedIp: HOST_IP,
+        }
+      ],
+      enableUdp: true,
+      enableTcp: true,
+      preferUdp: true,
+    }
+
+    let transport = await router.createWebRtcTransport(webRtcTransport_options);
+
+    transport.on('dtlsstatechange', dtlsState => {
+      if (dtlsState === 'closed') {
+        transport.close()
+      }
+    })
+
+    transport.on('close', () => {
+      console.log('transport closed')
+    })
+
+    callback({
+      params: {
+        id: transport.id,
+        iceParameters: transport.iceParameters,
+        iceCandidates: transport.iceCandidates,
+        dtlsParameters: transport.dtlsParameters,
+      }
+    })
+
+    return transport;
+  } catch (error) {
+    console.log(error)
+    callback({
+      params: {
+        error: error
+      }
+    })
+  }
+}
+
+const startRecord = async (producer, channelSlug, socketId) => {
+  let recordInfo = await publishProducerRtpStream(producer);
+
+  recordInfo.fileName = channelSlug + "-hls";
+  const options = {
+    "rtpParameters": recordInfo,
+    "format": "hls"
+  }
+  processWriteHLS[socketId] = new FFmpeg(options);
+}
+
+const publishProducerRtpStream = async (producer) => {
+  const rtpTransportConfig = config.plainRtpTransport;
+
+  const rtpTransport = await router.createPlainTransport(rtpTransportConfig)
+  const remoteRtpPort = await getPort();
+
+  let remoteRtcpPort;
+  if (!rtpTransportConfig.rtcpMux) {
+    remoteRtcpPort = await getPort();
+  }
+
+  await rtpTransport.connect({
+    ip: '127.0.0.1',
+    port: remoteRtpPort,
+    rtcpPort: remoteRtcpPort
+  });
+
+  const codecs = [];
+  const routerCodec = router.rtpCapabilities.codecs.find(
+    codec => codec.kind === producer.kind
+  );
+  codecs.push(routerCodec);
+  const rtpCapabilities = {
+    codecs,
+    rtcpFeedback: []
+  };
+
+  const rtpConsumer = await rtpTransport.consume({
+    producerId: producer.id,
+    rtpCapabilities,
+    paused: true
+  });
+
+  setTimeout(async () => {
+    rtpConsumer.resume();
+    rtpConsumer.requestKeyFrame();
+  }, 1000);
+
+  return {
+    remoteRtpPort,
+    remoteRtcpPort,
+    localRtcpPort: rtpTransport.rtcpTuple ? rtpTransport.rtcpTuple.localPort : undefined,
+    rtpCapabilities,
+    rtpParameters: rtpConsumer.rtpParameters
+  };
+};
---
+++ src/ffmpeg.js
@@ -0,0 +1,154 @@
+const child_process = require('child_process');
+const { EventEmitter } = require('events');
+const { createSdpText } = require('./sdp.js');
+const { convertStringToStream ,getOS} = require('./utils.js');
+const path = require("path")
+const Logger = require('./logger.js')
+const fs = require('fs');
+// console.log('path', path.resolve('../files'));
+const RECORD_FILE_LOCATION_PATH = process.env.RECORD_FILE_LOCATION_PATH || path.resolve('../server/files');
+
+const myOS = getOS()
+module.exports = class FFmpeg {
+  constructor(options) {
+    const { rtpParameters, format } = options;
+    this.format = format;
+    this._rtpParameters = rtpParameters;
+    this._process = null;
+    this._observer = new EventEmitter();
+    this.Logger = new Logger(`log.txt`).getlog()
+    this.formats = {
+      "mp3": this._audioArgs,
+      "hls": this._hlsArgs
+    }
+    this.args = this.formats[format]
+
+    this._createProcess();
+  }
+
+  _createProcess() {
+    const sdpString = createSdpText(this._rtpParameters);
+    const sdpStream = convertStringToStream(sdpString);
+
+    console.log('createProcess() [sdpString:%s]', sdpString);
+    this._process = child_process.spawn('ffmpeg', this._commandArgs);
+
+    if (this._process.stderr) {
+      this._process.stderr.setEncoding('utf-8');
+
+      this._process.stderr.on('data', data =>
+        // console.log('ffmpeg::process::data [data:%o]', data)
+        {  this.Logger.error(`ffmpeg ${data}`)}
+      );
+    }
+
+    if (this._process.stdout) {
+      this._process.stdout.setEncoding('utf-8');
+
+      this._process.stdout.on('data', data =>
+        // console.log('ffmpeg::process::data [data:%o]', data)
+        { this.Logger.info(`data ${data}`) }
+      );
+    }
+    this._process.on('message', message =>
+      console.log('ffmpeg::process::message [message:%o]', message)
+    );
+
+    this._process.on('error', error =>
+      console.error('ffmpeg::process::error [error:%o]', error)
+    );
+
+    this._process.once('close', () => {
+      console.log('ffmpeg::process::close');
+      this._observer.emit('process-close');
+    });
+
+    sdpStream.on('error', error =>
+      console.error('sdpStream::error [error:%o]', error)
+    );
+
+    // Pipe sdp stream to the ffmpeg process
+    sdpStream.resume();
+    sdpStream.pipe(this._process.stdin);
+  }
+
+  kill() {
+    console.log('kill() [pid:%d]', this._process.pid);
+    this._process.kill('SIGINT');
+  }
+
+  get _commandArgs() {
+    let commandArgs = [
+      '-loglevel',
+      'debug',
+      '-protocol_whitelist',
+      'pipe,udp,rtp',
+      '-fflags',
+      '+genpts',
+      '-f',
+      'sdp',
+      '-i',
+      'pipe:0'
+    ];
+    commandArgs = commandArgs.concat(this.args);
+
+    let filePath = ""
+    if (this.format == "mp3") {
+      if (myOS == "Windows") {
+        filePath = `${RECORD_FILE_LOCATION_PATH}\\mp3\\${this._rtpParameters.fileName}.mp3`
+      }
+      else {
+        filePath = `${RECORD_FILE_LOCATION_PATH}/mp3/${this._rtpParameters.fileName}.mp3`
+      }
+      commandArgs = commandArgs.concat([
+      filePath
+      ]);
+    }
+    else if (this.format == "hls") {
+      let folderPath =""
+      if (myOS == "Windows") {
+        folderPath = `${RECORD_FILE_LOCATION_PATH}\\hls\\${this._rtpParameters.fileName}`
+      }
+      else {
+        folderPath = `${RECORD_FILE_LOCATION_PATH}/hls/${this._rtpParameters.fileName}`
+      }
+
+      fs.mkdir(folderPath, (err) => {
+        if (err) {
+          // Handle the error if the folder creation failed
+          console.error('Error creating folder:', err);
+        } else {
+          // Folder created successfully
+          console.log('Folder created successfully:', folderPath);
+        }
+      });
+      commandArgs = commandArgs.concat([
+        `${folderPath}/${this._rtpParameters.fileName}.m3u8`
+      ]);
+    }
+    // console.log('arg', commandArgs);
+
+    return commandArgs;
+  }
+
+  get _audioArgs() {
+    return [
+      '-map',
+      '0:a:0',
+      '-strict', // libvorbis is experimental
+      '-2',
+      '-c:a',
+      'mp3',
+
+    ];
+  }
+  get _hlsArgs() {
+    return [
+      '-hls_time', '1',           
+      '-hls_list_size', '5',       
+      '-start_number', '1',       
+      '-f', 'hls',    
+      '-hls_flags', 'delete_segments+append_list'           
+    ];
+  }
+}
